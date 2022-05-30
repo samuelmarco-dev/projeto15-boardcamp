32994ae0530ecdcfa90c8da66323aff962c3c984
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-05-30 11:10:24 -0300

    feat: inserir e deletar aluguel em /rentals

--- src/controllers/alugueisControllers.js
+++ src/controllers/alugueisControllers.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+import joi from 'joi';
 import chalk from "chalk";
 
 import db from "../database.js";
@@ -26,9 +28,56 @@ async function listarAluguel(req, res){
 
 async function inserirAluguel(req, res){
     const { customerId, gameId, daysRented } = req.body;
+    const dataAluguel = dayjs().format('YYYY-MM-DD');
+    const [dataDevolucao, multaAtraso] = [null, null];
     
+    const schemaAluguel = joi.object({
+        customerId: joi.number().positive().min(1).required(),
+        gameId: joi.number().positive().min(1).required(),
+        daysRented: joi.number().positive().min(1).required()
+    });
+    const validation = schemaAluguel.validate({customerId, gameId, daysRented}, {abortEarly: false});
+
+    const {error} = validation;
+    if(error){
+        console.log(chalk.red('Erro de validação')); //apagar
+        return res.status(400).send(error.details.map(detail => detail.message));
+    }
+
     try {
-        
+        const cliente = await db.query(`
+            SELECT * FROM customers WHERE id = $1
+        `, [customerId]);
+        const [clienteId] = cliente.rows;
+        if(!clienteId) return res.status(404).send(`Customer with id: ${customerId} not found`);
+
+        const jogo = await db.query(`
+            SELECT * FROM games WHERE id = $1
+        `, [gameId]);
+        const [jogoId] = jogo.rows;
+        if(!jogoId) return res.status(404).send(`Game with id: ${gameId} not found`);
+
+        const aluguel = await db.query(`
+            SELECT * FROM rentals WHERE "gameId" = $1 AND "returnDate" IS NULL
+        `, [gameId]);
+
+        if(jogo.stockTotal > aluguel.rows.length){
+            const precoDia = await db.query(`
+                SELECT * FROM games WHERE id = $1
+            `, [gameId]);
+            const [precoDiaId] = precoDia.rows;
+            if(!precoDiaId) return res.status(404).send(`Game with id: ${gameId} not found`);
+
+            const precoTotal = precoDiaId.pricePerDay * daysRented;
+            await db.query(`
+                SELECT INTO rentals 
+                ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
+                VALUES ($1, $2, $3, $4, $5, $6, $7)
+            `, [customerId, gameId, dataAluguel, daysRented, dataDevolucao, precoTotal, multaAtraso]);
+            res.sendStatus(201);
+        }else{
+            return res.status(400).send(`Game with id: ${gameId} is out of stock`);
+        }
     } catch (error) {
         console.log(chalk.red('Erro de conexão')); //apagar
         res.sendStatus(500);
@@ -45,8 +94,21 @@ async function finalizarAluguel(req, res){
 }
 
 async function deletarAluguel(req, res){  
+    const {id} = req.params;
+    
     try {
+        const aluguel = await db.query(`
+            SELECT * FROM rentals WHERE id = $1
+        `, [id]);
+        const [aluguelId] = aluguel.rows;
         
+        if(!aluguelId) return res.status(404).send(`Rental with id: ${id} not found`);
+        if(aluguelId.returnDate !== null) return res.status(400).send(`Rental with id: ${id} already returned`);
+
+        await db.query(`
+            DELETE FROM rentals WHERE id = $1
+        `, [id]);
+        res.sendStatus(200);
     } catch (error) {
         console.log(chalk.red('Erro de conexão')); //apagar
         res.sendStatus(500);
